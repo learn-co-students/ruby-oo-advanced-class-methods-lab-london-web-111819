676b068a6d50537ffade94d39721d4eba83f16cf
Matthew <matthew@Matthews-MacBook-Pro.local>
2019-11-21 18:04:38 +0000

    4 errors left

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -9,5 +11,45 @@ class Song
   def save
     self.class.all << self
   end
-adsasdasdasdas
+
+  def self.create
+    new_song = Song.new
+    @@all.push(new_song)
+    new_song
+  end
+
+  def self.new_by_name(string)
+    new_song = Song.new
+    new_song.name = string
+    new_song
+  end
+
+  def self.create_by_name(string)
+    new_song = Song.new
+    new_song.name = string
+    @@all.push(new_song)
+    new_song
+  end
+
+  def self.find_by_name(target)
+    @@all.find do |song|
+      song.name == target
+    end
+  end
+
+  def self.find_or_create_by_name(string)
+    if !find_by_name(string)
+      create_by_name(string)
+    else
+      find_by_name(string)
+    end
+  end
+
+  def self.alphabetical
+    
+  end
+
+  def self.new_from_filename(format)
+    
+  end
 end
